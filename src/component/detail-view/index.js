4de01717e62f8952d5403ca4d36f64ad2653172b
Joshua Evans <skyfriends1@gmail.com>
2017-09-28 22:21:30 -0700

    got detail page working minimally

--- src/component/detail-view/index.js
+++ src/component/detail-view/index.js
@@ -1,14 +1,117 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { GridList, GridTile } from 'material-ui';
-import NavContainer from '../nav-container';
-import SidebarContainer from '../sidebar-container';
 
 class DetailView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { backgroundColor: null };
+  }
   render() {
+    console.log(this.props.state.colorData);
     return (
       <div>
-        <NavContainer />
-        <SidebarContainer />
+        <GridList
+          cols={1}
+          cellHeight={400}
+          padding={40}
+          style={{
+            position: 'absolute',
+            right: '2%',
+            top: '15%',
+            width: '78%',
+          }}
+        >
+          <GridTile
+            style={{
+              border: '1px solid #D6D8D8',
+              borderRadius: '10px',
+              backgroundColor: this.props.backgroundColor,
+            }}
+            titleBackground={'#fff'}
+            titleStyle={{
+              color: '#373C3C',
+              fontFamily: 'Source Serif Pro',
+            }}
+            title={this.props.backgroundColor}
+          />
+        </GridList>
+        <GridList
+          cols={5}
+          padding={40}
+          style={{
+            position: 'absolute',
+            right: '2%',
+            top: '75%',
+            width: '78%',
+          }}
+        >
+          <GridTile
+            style={{
+              border: '1px solid #D6D8D8',
+              borderRadius: '10px',
+              backgroundColor: this.props.state.colorData[5].name,
+            }}
+            titleBackground={'#fff'}
+            titleStyle={{
+              color: '#373C3C',
+              fontFamily: 'Source Serif Pro',
+            }}
+            title={this.props.state.colorData[5].name}
+          />
+          <GridTile
+            style={{
+              border: '1px solid #D6D8D8',
+              borderRadius: '10px',
+              backgroundColor: this.props.state.colorData[6].name,
+            }}
+            titleBackground={'#fff'}
+            titleStyle={{
+              color: '#373C3C',
+              fontFamily: 'Source Serif Pro',
+            }}
+            title={this.props.state.colorData[6].name}
+          />
+          <GridTile
+            style={{
+              border: '1px solid #D6D8D8',
+              borderRadius: '10px',
+              backgroundColor: this.props.state.colorData[7].name,
+            }}
+            titleBackground={'#fff'}
+            titleStyle={{
+              color: '#373C3C',
+              fontFamily: 'Source Serif Pro',
+            }}
+            title={this.props.state.colorData[7].name}
+          />
+          <GridTile
+            style={{
+              border: '1px solid #D6D8D8',
+              borderRadius: '10px',
+              backgroundColor: this.props.state.colorData[8].name,
+            }}
+            titleBackground={'#fff'}
+            titleStyle={{
+              color: '#373C3C',
+              fontFamily: 'Source Serif Pro',
+            }}
+            title={this.props.state.colorData[8].name}
+          />
+          <GridTile
+            style={{
+              border: '1px solid #D6D8D8',
+              borderRadius: '10px',
+              backgroundColor: this.props.state.colorData[9].name,
+            }}
+            titleBackground={'#fff'}
+            titleStyle={{
+              color: '#373C3C',
+              fontFamily: 'Source Serif Pro',
+            }}
+            title={this.props.state.colorData[9].name}
+          />
+        </GridList>
       </div>
     );
   }
--- src/component/list-view/index.js
+++ src/component/list-view/index.js
@@ -2,15 +2,22 @@ import React from 'react';
 import superagent from 'superagent';
 import Pagination from 'react-js-pagination';
 import { GridList, GridTile } from 'material-ui';
-import NavContainer from '../nav-container';
-import SidebarContainer from '../sidebar-container';
+
+import DetailView from '../detail-view';
 import '../../style/main.scss';
 
 class ListView extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { colorData: [], activePage: 1 };
+    this.state = {
+      colorData: [],
+      activePage: 1,
+      selectedColor: null,
+      detail: false,
+      list: true,
+    };
     this.handlePagination = this.handlePagination.bind(this);
+    this.handleDetailedView = this.handleDetailedView.bind(this);
   }
   componentWillMount() {
     superagent
@@ -26,54 +33,87 @@ class ListView extends React.Component {
       .then(res => this.setState({ colorData: res.body }));
   }
 
+  handleDetailedView(e) {
+    this.setState({
+      selectedColor: e.target.style.backgroundColor,
+      detail: !this.state.detail,
+      list: !this.state.list,
+    });
+  }
+
   render() {
     return (
       <div>
-        <NavContainer />
-        <div>
-          <SidebarContainer />
-        </div>
-        <div>
-          <GridList
-            cols={4}
-            padding={40}
-            style={{
-              position: 'absolute',
-              right: '2%',
-              top: '15%',
-              width: '78%',
-            }}
-          >
-            {this.state.colorData.map(color => (
-              <GridTile
-                key={color._id}
-                style={{
-                  border: '1px solid #D6D8D8',
-                  borderRadius: '10px',
-                  backgroundColor: color.name,
-                }}
-                titleBackground={'#fff'}
-                titleStyle={{
-                  color: '#373C3C',
-                  fontFamily: 'Source Serif Pro',
-                }}
-                title={color.name}
-              />
-            ))}
-          </GridList>
-          <Pagination
-            activePage={this.state.activePage}
-            itemsCountPerPage={12}
-            totalItemsCount={9852}
-            pageRangeDisplayed={8}
-            onChange={this.handlePagination}
-            hideNavigation
-            itemClass="page-number"
-            linkClass="page-link"
-            activeClass="page-number-active"
-            activeLinkClass="page-link-active"
-          />
-        </div>
+        {this.state.list ? (
+          <div>
+            <GridList
+              cols={4}
+              padding={40}
+              style={{
+                position: 'absolute',
+                right: '2%',
+                top: '15%',
+                width: '78%',
+              }}
+            >
+              {this.state.colorData.map(color => (
+                <GridTile
+                  key={color._id}
+                  style={{
+                    border: '1px solid #D6D8D8',
+                    borderRadius: '10px',
+                    backgroundColor: color.name,
+                  }}
+                  titleBackground={'#fff'}
+                  titleStyle={{
+                    color: '#373C3C',
+                    fontFamily: 'Source Serif Pro',
+                  }}
+                  title={color.name}
+                  onClick={this.handleDetailedView}
+                />
+              ))}
+            </GridList>
+            <Pagination
+              activePage={this.state.activePage}
+              itemsCountPerPage={12}
+              totalItemsCount={9852}
+              pageRangeDisplayed={8}
+              onChange={this.handlePagination}
+              hideNavigation
+              itemClass="page-number"
+              linkClass="page-link"
+              activeClass="page-number-active"
+              activeLinkClass="page-link-active"
+            />
+          </div>
+        ) : null}
+        {this.state.detail ? (
+          <div>
+            <DetailView
+              backgroundColor={this.state.selectedColor}
+              state={this.state}
+            />
+            <div
+              style={{
+                position: 'relative',
+                width: '30%',
+                left: '45%',
+                fontSize: '1em',
+                border: '2px solid #D6D8D8',
+                textAlign: 'center',
+                padding: '2px',
+                borderRadius: '8px',
+                backgroundColor: '#fff',
+                fontFamily: 'Source Serfif Pro',
+                fontWeight: 700,
+              }}
+              onClick={this.handleDetailedView}
+            >
+              <p>Clear</p>
+            </div>
+          </div>
+        ) : null}
       </div>
     );
   }
